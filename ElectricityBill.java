a5fe2703f2c2638766b409fef3902b667e1eece2
Singh Nitesh kumar Ravindra <126550402+Nitesh-Singh347@users.noreply.github.com>
2023-12-12 22:19:37 +0530

    Add files via upload

---
+++ ElectricityBill.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.electricitybill;
+
+/**
+ *
+ * @author Admin
+ */
+public class ElectricityBill {
+
+    public static void main(String[] args) {
+      Bill ob = new Bill();
+      ob.setVisible(true);
+    }
+}
